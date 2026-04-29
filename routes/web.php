48b5e1a7f5b57ca2392c87bf6685d27308df5656
Jossa7077 <santyjossa5@gmail.com>
2026-04-29 11:39:09 -0500

    Cambios_Seguridad

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PaginaController;
-use App\Http\Controllers\PqrsController;
 
-Route::get('/', [PaginaController::class, 'inicio'])->name('inicio'); 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/menu', [PaginaController::class, 'menu'])->name('menu'); 
-Route::get('/nosotros', [PaginaController::class, 'nosotros'])->name('nosotros'); 
- 
-Route::get('/contacto', [PaginaController::class, 'contacto'])->name('contacto');
-Route::post('/pqrs',[PqrsController::class,'store'])->name('pqrs.store');
-Route::get('/mensajes', [PqrsController::class, 'index'])->name('mensajes.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
